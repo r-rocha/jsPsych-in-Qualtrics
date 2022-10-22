b641737c5ac823d1dc4b77d76829977ba5f18bff
Roberto Rocha <40395270+r-rocha@users.noreply.github.com>
2022-10-22 12:28:46 -0300

    Update demo-flanker-qualtrics.js

--- flanker/demo-flanker-qualtrics.js
+++ flanker/demo-flanker-qualtrics.js
@@ -11,7 +11,7 @@ Qualtrics.SurveyEngine.addOnload(function () {
 
     /* Change 2: Defining and load required resources */
     // https://cdn.jsdelivr.net/gh/<github-username>/<repository-name>/<experiment-folder>
-    var task_github = "https://cdn.jsdelivr.net/gh/kywch/jsPsych-in-Qualtrics/flanker/";
+    var task_github = "https://cdn.jsdelivr.net/gh/r-rocha/jsPsych-in-Qualtrics/flanker/";
 
     // requiredResources must include all the JS files that demo-simple-rt-task-transformed.html uses.
     var requiredResources = [
